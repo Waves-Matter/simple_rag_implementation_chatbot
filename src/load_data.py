c9fd76e4597dd24ccdb76d86c07caf950e7607a0
Waves-Matter <ugnsili@gmail.com>
2025-04-16 20:49:38 +0300

    fixed splitting

--- src/load_data.py
+++ src/load_data.py
@@ -1,8 +1,6 @@
 
 import os
 
-from dotenv import load_dotenv
-from sentence_transformers import SentenceTransformer
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import JSONLoader
 from langchain_community.vectorstores import Chroma
@@ -10,16 +8,23 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 file_path = "docs\hc_articles.json"
 
-loader = JSONLoader(file_path=file_path, jq_schema=".[]", text_content=False)
+loader = JSONLoader(file_path=file_path, jq_schema='.[]', text_content=False)
 docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
 
 model_name = "all-MiniLM-L6-v2"
 embeddings = HuggingFaceEmbeddings(model_name=model_name)
 
-vectorstore = Chroma.from_documents(documents=splits, 
+vectorstore = Chroma.from_documents(documents=docs, 
                                     embedding=embeddings)
 
 retriever = vectorstore.as_retriever()
+
+
+query = "how do i install nodVPN?"
+
+# Perform a similarity search
+results = vectorstore.similarity_search(query, )
+
+for result in results:
+    print(result.page_content)
+    print("--------------")
