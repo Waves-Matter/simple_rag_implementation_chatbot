be1432c933498b42d81febb4ae2aa9599dba35db
Waves-Matter <ugnsili@gmail.com>
2025-04-17 18:06:55 +0300

    libreries hell

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from retriever import get_retriever
+import os
 
+from retriever import get_retriever
 
 file_path = "docs\hc_articles.json"
 embed_model_name = "thenlper/gte-small" 
-#other possible model: all-MiniLM-L6-v2"
+#other possible models:: "all-MiniLM-L6-v2"; "thenlper/gte-large"
 
 my_retriever = get_retriever(file_path, embed_model_name)
 
@@ -12,3 +13,6 @@ results = my_retriever.get_relevant_documents(query)
 for result in results:
     print(result)
     print("---------------------")
+
+#llm_model_name = "HuggingFaceH4/zephyr-7b-beta"
+
